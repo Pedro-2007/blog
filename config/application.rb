9dde07497312e67f383bd2e5e51dcdde1e519a41
Rodrigo Aquino <rodrigo.aquino@fullhealthmedical.com>
2024-02-10 19:17:29 -0300

    Display comment created_at

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module Blog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Brazil time
+    config.time_zone = "Brasilia"
+
+    # I18n
+    config.i18n.default_locale = "pt-BR"
   end
 end
