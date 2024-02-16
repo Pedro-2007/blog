92c7b2b5a9f02324255ba063ac66fae30658d7d2
Pedro Rodrigues <Pedrorodl07@gmail.com>
2024-02-16 17:32:28 -0300

    iniciando o login e logout

---
+++ app/controllers/Sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+    def new
+    end
+  
+    def create
+      user = User.find_by(email: params[:email])
+  
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to root_path, notice: 'Login bem-sucedido!'
+      else
+        flash.now.alert = 'Email ou senha inválidos'
+        render :new
+      end
+    end
+  
+    def destroy
+      reset_session
+      redirect_to root_path, notice: 'Logout realizado com sucesso!'
+    end
+  end
+  
