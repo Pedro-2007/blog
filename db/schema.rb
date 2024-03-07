66d9ee649f60f7ea25cce4632999ef631fbffa91
Pedro Rodrigues <Pedrorodl07@gmail.com>
2024-03-07 14:08:48 -0300

    add login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_10_215311) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_15_175232) do
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
     t.text "body"
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_10_215311) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -33,7 +34,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_10_215311) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   add_foreign_key "comments", "posts"
 end
