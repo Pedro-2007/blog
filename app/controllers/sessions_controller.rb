2e5d76f96a909e312792c7639239284885356ba1
Pedro Rodrigues <Pedrorodl07@gmail.com>
2024-03-07 14:46:17 -0300

    alteração

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,22 @@
 class SessionsController < ApplicationController
     def new
-      @user = User.new
     end
   
     def create
-      user = User.find_by(email: user_params[:email])
+      user = User.find_by(email: params[:email])
   
-      if user && user.authenticate(user_params[:password])
+      if user && user.authenticate(params[:password])
         session[:user_id] = user.id
         redirect_to root_path, notice: 'Login bem-sucedido!'
       else
         flash.now.alert = 'Email ou senha inválidos'
-        @user = User.new
-        render :new, status: :unprocessable_entity
+        render :new
       end
     end
   
     def destroy
-      session[:user_id] = nil
-      redirect_to new_sessions_path, notice: 'Logout bem-sucedido!'
-    end
-
-    def user_params
-      params.require(:user)
+      reset_session
+      redirect_to root_path, notice: 'Logout realizado com sucesso!'
     end
   end
   
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -23,4 +23,4 @@ class UsersController < ApplicationController
         else
           render :new
     end
-  end
+end
