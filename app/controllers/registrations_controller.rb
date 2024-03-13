ef570e9a01afb7a15c8c234aa5d4969199ac5c9e
Pedro Rodrigues <Pedrorodl07@gmail.com>
2024-03-13 18:44:00 -0300

    register with name

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -16,7 +16,7 @@ class RegistrationsController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
   end
 end
   
