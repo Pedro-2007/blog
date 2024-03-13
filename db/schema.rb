06bf980eab53efcecb28124ccf4d28df41c9e76a
Pedro Rodrigues <Pedrorodl07@gmail.com>
2024-03-13 18:32:37 -0300

    comments user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_13_200351) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_13_205753) do
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -37,5 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_13_200351) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
